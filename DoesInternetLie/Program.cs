cf128a758c74ce56dd0b28b0f4915b1137ea8996
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-10 14:18:26 +0100

    InsertedActualProgram

--- DoesInternetLie/Program.cs
+++ DoesInternetLie/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using static System.Console;
+using static System.Convert;
 
 namespace DoesInternetLie
 {
     class Program
     {
+        private static void CheckType(object a)
+        {
+            WriteLine($"{a.GetType()}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string b = "5";
+            CheckType(b);
+            WriteLine("Yes it does, sometimes");
+            ReadKey();
         }
     }
 }
